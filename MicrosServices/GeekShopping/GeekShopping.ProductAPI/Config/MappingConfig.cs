3849ec1162e14a7a2aa71557ce5c772de86f10d5
aricsondr <aricsondr@gmail.com>
2022-10-23 11:44:03 +0100

    Creating the first microservice

---
+++ MicrosServices/GeekShopping/GeekShopping.ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
+
+namespace GeekShopping.ProductAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingconfig = new MapperConfiguration(Config => 
+            {
+                Config.CreateMap<ProductVO, Product>();
+                Config.CreateMap<Product, ProductVO>();
+            });
+            return mappingconfig;
+        }
+    }
+}
